2db928b437b0a810d216ab5de68068b31de47a44
Jason Carvalho <jase@jasoncarvalho.com>
2018-12-03 16:29:19 +0000

    search functions: createQueryNode()

--- cosa/search/functions.py
+++ cosa/search/functions.py
@@ -1,4 +1,40 @@
-def createQueryNode():
+
+def createQueryNode(input):
+    import sys
+    from os.path import dirname, join, abspath
+    sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+    from cosa.dbpedia.DBPedia import DBPedia
+    from cosa.nlp.functions import text2terms
+    node = {}
+    node['label'] = input
+    node['terms'] = {}
+    node['entities'] = {}
+    myDBPedia = DBPedia('http://anne.kmi.open.ac.uk/rest/annotate', 'http://dbpedia.org/sparql')
+    spotlight = myDBPedia.spotlight(input, 0.1)
+    for item in spotlight:
+        node['entities'][item] = {}
+        node['entities'][item]['score'] = spotlight[item]['score']
+        node['entities'][item]['types'] = []
+        node['entities'][item]['subjects'] = []
+        dbpSubjsTypes = myDBPedia.getSubjTypeURIs(item) #returns an array
+        for arrayItem in dbpSubjsTypes:
+            if arrayItem['type'] == 'S':
+                node['entities'][item]['subjects'].append(arrayItem['uri'])
+            elif arrayItem['type'] == 'T':
+                node['entities'][item]['types'].append(arrayItem['uri'])
+            else:
+                pass #it wasn't a 'subject' or 'Type', something went wrong
+
+    #Now create terms from label
+    for term in text2terms(node['label']):
+        node['terms'][term] = {
+            'term': term,
+            'score': 1.0
+        }
+
+    return node
+
+def addNodeToQueryGraph():
     return True
 
 def createQueryGraph():
@@ -7,8 +43,7 @@ def createQueryGraph():
 def matchNodes():
     return True
 
-def addNodeToQueryGraph():
-    return True
+
 
 def testFunction():
     print ("Hello")
