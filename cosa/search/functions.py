8731dfb5ec4607ad83fee2581dfbfa808e0966a7
Jason Carvalho <jase@jasoncarvalho.com>
2018-12-03 12:02:20 +0000

    Building search functions

---
+++ cosa/search/functions.py
@@ -0,0 +1,14 @@
+def createQueryNode():
+    return True
+
+def createQueryGraph():
+    return True
+
+def matchNodes():
+    return True
+
+def addNodeToQueryGraph():
+    return True
+
+def testFunction():
+    print ("Hello")
