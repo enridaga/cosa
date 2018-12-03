eb1f4b696c7c30ce268fd7a712360c1fc89257dc
Jason Carvalho <jase@jasoncarvalho.com>
2018-12-03 12:02:12 +0000

    Building search functions

--- scripts/search.py
+++ scripts/search.py
@@ -4,9 +4,8 @@ def main():
     import sys
     from os.path import dirname, join, abspath
     sys.path.insert(0, abspath(join(dirname(__file__), '..')))
-    import cosa
-
-    print cosa.search.createQueryNode()
+    from cosa.search.functions import createQueryNode
+    print createQueryNode()
 
 
 if __name__ == "__main__":
