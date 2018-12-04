ba9fdaa6d72e44d4acdea576fc4eb80eda6888ba
enridaga <enricodaga@gmail.com>
2018-12-04 09:48:46 +0000

    added comment

--- cosa/nlp/Model.py
+++ cosa/nlp/Model.py
@@ -49,7 +49,8 @@ class Model:
             L.error( "Exiting: " + str(e))
             sc.stop()
             exit()
-    
+
+    """Returns a list of tuples (term, score) """
     def similarToTerm(self, term, num):
         first=lambda x: x[0]
         # print "Looking for syns of " + depos(key)
