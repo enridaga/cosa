4184c2904e5ebfe3f946a7308c702c4606970f30
enridaga <enricodaga@gmail.com>
2018-12-05 16:33:16 +0000

    Added try/catch block to script

--- scripts/search.py
+++ scripts/search.py
@@ -24,33 +24,40 @@ def _search():
     print 'Graph loaded.'
 
     print 'Loading model...'
-    model = Model('/Users/jc33796/Documents/UKPostings/Data/Gutenberg2Vec/word2vec.model')
-
+    #model = Model('/Users/jc33796/Documents/UKPostings/Data/Gutenberg2Vec/word2vec.model')
+    model = Model('/Users/ed4565/Development/led-discovery/data/analysis/gutenberg2vec/word2vec.model')
+    
     print 'Model loaded.'
 
     while(True):
         print 'Search:'
-        text = sys.stdin.readline()
-        text = text.split(':')
-        method = 'entities'
-        if len(text) > 1:
-            method = text[0]
-            text = text[1]
-        print 'Searching with method ' + method + '...'
-        rs = ResultSet()
-        if method == 'terms':
-            rs = searchGraph(text, g, method, model, 1000)
-        else:
-            rs = searchGraph(text, g, method)
+        try:
+            text = sys.stdin.readline()
+            text = text.split(':')
+            method = 'entities'
+            if len(text) > 1:
+                method = text[0]
+                text = text[1]
+            print 'Searching with method ' + method + '...'
+            rs = ResultSet()
+            if method == 'terms':
+                rs = searchGraph(text, g, method, model, 2)
+            else:
+                rs = searchGraph(text, g, method)
 
-        print '**************'
-        print 'Top 10 results by score'
-        rs.printTopScores(10);
+            print '**************'
+            print 'Top 10 results by score'
+            rs.printTopScores(10);
 
-        print '**************'
-        print 'Top 10 results by normalised(by depth) score'
-        rs.printTopNScores(10);
-
+            print '**************'
+            print 'Top 10 results by normalised(by depth) score'
+            rs.printTopNScores(10);
+        except Exception as e:
+            print e
+            pass #print " - not found (exception)"
+        except Error as r:
+            print r
+            pass #print " - error"
     
 def main():
     if len(sys.argv) == 1:
